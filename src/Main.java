1fb722712d6f37e884ce071a4b70715af0325f0d
Ivan-netizen <55799873+Ivan-netizen@users.noreply.github.com>
2021-11-25 13:29:56 -0600

    Generé main

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author ivanj
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
