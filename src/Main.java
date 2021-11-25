e8e23cec592c207882eb37427590ffc7cfd7ccd0
Ivan-netizen <55799873+Ivan-netizen@users.noreply.github.com>
2021-11-25 13:31:30 -0600

    cambié main

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola Ivan");
     }
     
 }
